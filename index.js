b175652ca9aa4b48dc38b4b872a5ae0366020d45
EdsonSoumay <69837656+EdsonSoumay@users.noreply.github.com>
2020-08-27 19:55:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+/*dasar-dasar pemrograman*/
+
+const firstName = "Edson "; 
+const MiddleName="Robert ";
+const lastName = "Soumay";
+
+let age = 20 ; //tipe data number 
+let status = "jomblo"; //tipe data string
+
+console.log(firstName); //output:Edson
+console.log(MiddleName);//:Robert
+console.log(lastName); // :Soumay
+console.log(age); //20
+console.log(status); //single
+
+const info = " My name is " + firstName + MiddleName + lastName + " I'm " + age + " years old, I'm " + status ; //menampung banyak tipe data   
+console.log(info);
