6f2c88729b01cb56858ed1354fb161bf76ebf47c
EdsonSoumay <s21810088@student.unklab.ac.id>
2020-08-27 18:15:51 +0800

    1st commit

---
+++ pertemuan_4/index.js
@@ -0,0 +1,21 @@
+let a =8;
+let b=3;
+let c=10;
+let d=5;
+
+
+let luasA= a*a;
+let luasA= b*b;
+let luasA= c*c;
+let luasA= d*d;
+
+function PenjumlahanDuaBujurSangkar(a,b){
+    let luasA= a*a;
+    let luasB= b*b;
+
+    let total = luasA+luasB;
+    
+    return total;
+}
+
+console.log(PenjumlahanDuaBujurSangkar(8,3));
