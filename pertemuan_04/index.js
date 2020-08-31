96d6cd5a26878f4de25d619b9b27b2dc7577f96c
EdsonSoumay <s21810088@student.unklab.ac.id>
2020-08-31 21:54:54 +0800

    1st commit

--- pertemuan_04/index.js
+++ pertemuan_04/index.js
@@ -1,4 +1,4 @@
-let a =8;
+let a =8; //pendeklarasian
 let b=3;
 let c=10;
 let d=5;
@@ -9,8 +9,8 @@ let luasA= b*b;
 let luasA= c*c;
 let luasA= d*d;
 
-function PenjumlahanDuaBujurSangkar(a,b){
-    let luasA= a*a;
+function PenjumlahanDuaBujurSangkar(a,b){  //fungsi dalam javascript
+    let luasA= a*a; //aritmatika sederhana
     let luasB= b*b;
 
     let total = luasA+luasB;
@@ -18,38 +18,4 @@ function PenjumlahanDuaBujurSangkar(a,b){
     return total;
 }
 
-console.log(PenjumlahanDuaBujurSangkar(8,3));
-
-/*  
-// contoh 
-let a = 8;
-let b = 3;
-let LuasA = a * a;
-let LuasB = b * b;
-let total = LuasA + LuasB;
-console.log(total);
-
-/* 
-1. fungsi expression
-const penjumlahan = function(opsional) (a,b)
-{
-    let total;
-    total = a + b;
-    return total;(opsional)
-}
-const hasil = penjumlahan (8, 3); memanggil fungsi
-console.log(hasil)
--lebih mudah dipahami
--lebih flexible karena adanya hoisting dimana fungsi pemanggilnya bisa di tulis dmna saja
-2. fungsi declaration
-const penjumlahan = (a,b)=parameter
-{
-    let total;
-    total = a + b;
-    return total;
-}
-const hasil = penjumlahan (8, 3);= argumen
-console.log(hasil)
--harus di definisikan dahulu baru bisa di panggil
--lebih powerfull
--built functio
+console.log(PenjumlahanDuaBujurSangkar(8,3)); //memanggil fungsi
