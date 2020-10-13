9f49391c315192567c8d6d69561409fbd12bd9f4
EdsonSoumay <s21810088@student.unklab.ac.id>
2020-10-13 14:45:41 +0800

    1st commit

---
+++ pertemuan_16/Hello.js
@@ -0,0 +1,13 @@
+//import {world} from './world.js';
+
+//export const numbers = [1,2,3,4,5];
+
+
+/*
+export const hello = (user) => {
+  world();
+  console.log ('hello, ${user}');  
+}
+*/
+
+//export default hello;
---
+++ pertemuan_16/index.js
@@ -0,0 +1,9 @@
+//Modularisasi Javascript: export dan import
+//import { hello, world,numbers} from './Hello.js'
+//import = as helper from './hello.js
+//hello('John Doe');
+//world();
+//console.log(numbers)
+//func1('John Doe');
+//helper.world();
+//console.log(helper.numbers)
