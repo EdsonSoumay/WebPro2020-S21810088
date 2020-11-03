c2a872fb1b34cc34eda17bf5811393ec45772a4f
EdsonSoumay <s21810088@student.unklab.ac.id>
2020-11-03 14:27:38 +0800

    1st commit

--- Tes02/index.js
+++ Tes02/index.js
@@ -1,8 +1,10 @@
-//BAGIAN 1--------
+//BAGIAN 1
 
 
-//jawaban no 1---------------------
+
 /*
+//jawaban no 1
+
 const calculateAge = (birthYear)=> {
 }
  
@@ -23,7 +25,8 @@ yearUntilRetirement(
 */
 
 /*
-// jawaban no 2-------------------------
+// jawaban no 2
+
    const addNumber = (a, b, c, d, e, f, g) => {
     let numbers = [a, b, c, d, e, f, g];
     let sum = 0;
@@ -41,7 +44,8 @@ addNumber(1,2,3,4,5,6,7)
 
 
 /*
-//jawaban no 3----------------------
+//jawaban no 3
+
 let phi = 3.14;
 let power = 2;
 let radius = 0;
@@ -59,7 +63,8 @@ console.log('area with 21 radius: ' + area21 + ', and area with 7 radius: ' + ar
 
 
 /* 
-//jawaban no 4----------------
+//jawaban no 4
+
 const makeAjaxRequest = (url, method) => {
     if(!method){
            method = `GET`
@@ -72,10 +77,11 @@ makeAjaxRequest(`www.google.com`);
 
 
 
-// BAGIAN 2-------------
-// jawaban no 1----
+// BAGIAN 2
 
 /*
+// jawaban no 1
+
 const WarnaKesukaan =(warna="putih") =>{
     return console.log(`Warna kesukaan saya adalah ${warna}`)
     }
@@ -83,7 +89,7 @@ const WarnaKesukaan =(warna="putih") =>{
     WarnaKesukaan('biru');
     
 
-//jawaban no 2-----
+//jawaban no 2
 
 let namabuah1 = ["apel", "mangga"];
 let namabuah2 = ["sirsak", "semangka"];
