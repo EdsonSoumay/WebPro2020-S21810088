dfb35da49ca958d0bcf040d858cd59d7b5f27e8b
EdsonSoumay <s21810088@student.unklab.ac.id>
2020-11-20 13:51:30 +0800

    pertemuan 25

--- pertemuan_20/exercise04/src/App.js
+++ pertemuan_20/exercise04/src/App.js
@@ -1,7 +1,7 @@
 //import Header from './components/Header';
 import './App.css';
-import React, {Component} from "react";
-import Card from './components/card-pertemuan21';
+//import React, {Component} from "react";
+//import Card from './components/card-pertemuan21';
 //import axios from "axios";
 //import {person} from './utils/person-pertemuan21';
 //import Counter from './components/counter-pertemuan22';
@@ -11,9 +11,61 @@ import Card from './components/card-pertemuan21';
 //import Ttl from './components/Ttl';
 //import Alamat from './components/Alamat';
 
+/*
+// ---------------------------------------PERTEMUAN 25 --------------------------------------------
+import React from 'react'
+//import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { Home, Login, Register } from './pages'
+
+
+const App = () =>{
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/home">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/login">
+                                Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/register">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/home">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default App
+
+
+*/
+
 
 /*
-//PERTEMUAN 24
+//------------------------------------PERTEMUAN 24----------------------------------------------
 class App extends Component{
  
   state = {
@@ -52,16 +104,17 @@ class App extends Component{
 
 */
 
+
 /*
-PERTEMUAN 23 NO CLASS
-sir suruh melanjutkan tugas React Aplikasi
+------------------------------------PERTEMUAN 23 NO CLASS----------------------------
+sir suruh melanjutkan tugas React Aplikasi Dragon ball
 */
 
 
 
 
 /*
-//PERTEMUAN 22
+//-----------------------------------PERTEMUAN 22--------------------------------------
 function App() {
   return( 
     <>
@@ -75,7 +128,7 @@ function App() {
 
 
 /*
-//PERTEMUAN 21
+//----------------------------------PERTEMUAN 21----------------------------------
 function App() {
   return( 
     <>
@@ -98,7 +151,9 @@ function App() {
 
 
   /*
-//EXERCISE 04
+//-----------------------------------EXERCISE 04-----------------------------------------------
+
+
 function App() {
   return( 
     <>
